5dee087d5b36954c52c5eaa104a3135806e3685e
Mohammadreza <mohammmadreza.ziadzadeh@gmail.com>
2020-12-06 22:38:23 +0330

    :construction: Add event manager util function

---
+++ src/utils/emitter.ts
@@ -0,0 +1,47 @@
+import { Events } from './types'
+
+// An event handler will take an event argument
+// and should not return a value
+export type Handler<T = any> = (event: T) => void
+
+interface Emitter {
+  on<T = any>(event: Events, handler: Handler<T>): void
+
+  emit<T = any>(event: Events, args?: T): void
+
+  off(event: Events): void
+}
+
+export const emitter = (): Emitter => {
+  const events = new Map()
+
+  return {
+    /**
+     * Register an event handler for the given event name.
+     * @param {Events} type Type of event to listen for
+     * @param {Function} callback Function to call in response to given event
+     */
+    on<T = any>(event: Events, callback: Handler<T>) {
+      if (!events.has(event)) events.set(event, [])
+      events.get(event).push(callback)
+    },
+
+    /**
+     * Invoke all handlers for the given event name.
+     * @param {Events} type The event type to invoke
+     * @param {Any} args Any value (object is recommended and powerful), passed to each handler
+     */
+    emit<T = any>(event: Events, args: T) {
+      if (!events.has(event)) return
+      events.get(event).forEach((callback: Handler) => callback(args))
+    },
+
+    /**
+     * Register an event handler for the given event name.
+     * @param {Events} type Type of event to unregister `handler` from.
+     */
+    off(event: Events) {
+      events.delete(event)
+    },
+  }
+}
