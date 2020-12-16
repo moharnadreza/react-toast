f7e1b2044c8dbc6fa28b4e7a031cd76293489e45
Mohammadreza <moharnadreza@gmail.com>
2020-12-16 23:50:11 +0330

    :construction: Add new toast types

--- src/toast.ts
+++ src/toast.ts
@@ -1,17 +1,21 @@
 import { emitter, toaster, Toaster } from './utils'
 import { Events, ToastContent, ToastConfig } from './types'
 
-const applyToast = ({ content, type, config }: Toaster) =>
-  emitter.emit(
-    Events.SHOW,
-    toaster({
-      content,
-      type,
-      config,
-    })
-  )
+const applyToast = ({ ...toast }: Toaster) => emitter.emit(Events.SHOW, toaster({ ...toast }))
 
 const toast = (content: ToastContent, config?: ToastConfig) =>
   applyToast({ content, type: 'default', config })
+
+toast.success = (content: ToastContent, config?: ToastConfig) =>
+  applyToast({ content, type: 'success', config })
+
+toast.error = (content: ToastContent, config?: ToastConfig) =>
+  applyToast({ content, type: 'error', config })
+
+toast.info = (content: ToastContent, config?: ToastConfig) =>
+  applyToast({ content, type: 'info', config })
+
+toast.warn = (content: ToastContent, config?: ToastConfig) =>
+  applyToast({ content, type: 'warning', config })
 
 export { toast }
