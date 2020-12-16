0572483004db85f9e540da7d429714f2fe0c717f
Mohammadreza <moharnadreza@gmail.com>
2020-12-17 02:37:15 +0330

    :seedling: Add toast dispatcher util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './emitter'
 export * from './toaster'
+export * from './toastDispatcher'
---
+++ src/utils/toastDispatcher.ts
@@ -0,0 +1,22 @@
+import { emitter } from './'
+import { Events, Toast } from '../types'
+import { TAction } from '../hooks'
+
+interface IToastDispatcher {
+  dispatch: (value: TAction) => void
+  delay?: number
+}
+
+export const toastDispatcher = ({ dispatch, delay }: IToastDispatcher) => {
+  emitter.on(Events.SHOW, (toast: Toast) => {
+    dispatch({ type: 'ADD', toast })
+    if (delay)
+      setTimeout(() => {
+        dispatch({ type: 'REMOVE', id: toast.id })
+      }, delay)
+  })
+
+  emitter.on(Events.HIDE, (id: string) => dispatch({ type: 'REMOVE', id }))
+
+  emitter.on(Events.HIDE_ALL, () => dispatch({ type: 'REMOVE_ALL' }))
+}
