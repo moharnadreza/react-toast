7a938430a44025a92293202e27aa93c2b6521e9b
Mohammadreza <moharnadreza@gmail.com>
2020-12-17 02:51:21 +0330

    :seedling: Add toast icon

---
+++ src/components/Toast/ToastIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Success, Close, Info, Warning } from '..'
+import { Type } from '../../types'
+
+interface IToastIcon {
+  type: Type
+}
+
+export const toastIcon = ({ type }: IToastIcon) => {
+  switch (type) {
+    case 'error':
+      return <Close />
+    case 'info':
+      return <Info />
+    case 'warning':
+      return <Warning />
+    default:
+      return <Success />
+  }
+}
