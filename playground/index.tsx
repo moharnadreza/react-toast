4dba6f87bbebd69dc7365e285a8a8ea663580346
Mohammadreza <moharnadreza@gmail.com>
2020-12-18 02:06:28 +0330

    :basketball: Update playground

--- playground/index.tsx
+++ playground/index.tsx
@@ -2,25 +2,38 @@ import 'react-app-polyfill/ie11'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { ToastContainer, toast } from '../dist'
+import './style.css'
 
 const App = () => {
+  const morning = () => toast('Morning! Have a good day.')
+
+  const success = () => toast.success('Message sent successfully!')
+
+  const error = () => toast.error('Oops! Some error occurred.')
+
+  const info = () => toast.info('Do you know about this feature?')
+
+  const warn = () => toast.warn('This may be dangerous!')
+
+  const custom = () =>
+    toast('Hey! This is a custom toast.', {
+      backgroundColor: '#212121',
+      color: '#ffffff',
+    })
+
   return (
-    <div
-      style={{
-        padding: '0',
-        margin: '0',
-        fontFamily: 'Nunito',
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'black',
-      }}
-    >
-      <button onClick={() => toast('Hey there 👋')}>Say hi</button>
+    <>
+      <div className="controllers">
+        <button onClick={morning}>Default</button>
+        <button onClick={success}>Success</button>
+        <button onClick={error}>Error</button>
+        <button onClick={info}>Info</button>
+        <button onClick={warn}>Warning</button>
+        <button onClick={custom}>Custom</button>
+        <button onClick={() => toast.hideAll()}>Hide All</button>
+      </div>
       <ToastContainer />
-    </div>
+    </>
   )
 }
 
